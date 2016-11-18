38339a612c1256799c2d36fee7eee5326cc5f532
lev.ryskin@mail.ru <gbhfns1917@>
2016-11-18 14:08:22 +0300

    i'm emil

--- solar_model.py
+++ solar_model.py
@@ -14,13 +14,16 @@ def calculate_force(body, space_objects):
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
+    G = 6.7E-11
     body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        else:
+
+            r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+            body.Fx += 1  # FIXME: нужно вывести формулу...
+            body.Fy += 2  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
