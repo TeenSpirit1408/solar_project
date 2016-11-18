52088a35b8830e78f6361d6bd78db60f0d2a5482
lev.ryskin@mail.ru <gbhfns1917@>
2016-11-18 14:10:02 +0300

    Ex1 is dine

--- solar_input.py
+++ solar_input.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# license: GPLv3   prarparpap
+# license: GPLv3
 
 from solar_objects import Star, Planet
 
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            if object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -44,7 +48,15 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+    dates = line.split()
+    star.R = dates[1]
+    star.color = dates[2]
+    star.m = dates[3]
+    star.x = dates[4]
+    star.y = dates[5]
+    star.Vx = dates[6]
+    star.Vy = dates[7]
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +73,16 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+
+    dates = line.split()
+    planet.R = dates[1]
+    planet.color = dates[2]
+    planet.m = dates[3]
+    planet.x = dates[4]
+    planet.y = dates[5]
+    planet.Vx = dates[6]
+    planet.Vy = dates[7]
+
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
